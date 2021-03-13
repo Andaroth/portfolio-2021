8a0178d80dcf9d3942d12450011f3996bc1cc866
Anda <ax.fiolle@gmail.com>
2021-03-13 16:45:53 +0100

    UPDATE own content step1

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,78 @@
 <template>
   <v-app>
-    <v-app-bar
+    <v-app-bar class="topbar-slide"
       app
-      color="primary"
+      color="grey darken-4"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <a href="/" color="white" class="homepage-link">
+        <div class="d-flex align-center">
+          <v-img src="./assets/crane.jpg" class="shrink mr-2" width="40"
+            alt="Axel Fiolle"
+            style="border-radius: 50%;"
+          />
+          <h1>andaroth.art</h1>
+        </div>
+      </a>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://soundcloud.com/axel-andaroth"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">SoundCloud</span>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </v-btn>
+      <v-btn
+        href="https://github.com/Andaroth"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">GitHub</span>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </v-btn>
+      <v-btn
+        href="https://www.flickr.com/photos/axel-andaroth/"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Flickr</span>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </v-btn>
+      <v-btn
+        href="https://www.instagram.com/an.da.ro.th/"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Instagram</span>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </v-btn>
+      <v-btn
+        href="https://axelandaroth.wordpress.com/"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">WordPress</span>
+        <v-icon x-small>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
+    <v-main class="page-viewer">
+      <LandingPage/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import LandingPage from './components/LandingPage';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    LandingPage,
   },
 
   data: () => ({
@@ -58,3 +80,22 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss" scoped>
+  .page-viewer {
+    background-image: url('./assets/bg_clouds.jpg');
+    background-attachment: fixed;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+
+  .topbar-slide {
+    overflow-x: auto;
+  }
+
+  a {
+    color: #CC9A54 !important;
+    caret-color: #CC9A54 !important;
+    &.homepage-link { text-decoration: none; }
+  }
+</style>
